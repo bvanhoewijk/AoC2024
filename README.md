334932e5dd125760b53dd6450a9e92fb7981b50a
Bas van Hoewijk <b.vanhoewijk@enzazaden.nl>
2024-12-03 09:02:21 +0100

    day03

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## Day 01
 - Part 1: Sorting and comparing numbers
-- Part 2: Finding occurence and multiplying
+- Part 2: Finding occurences and multiply
 
 ## Day 02
 - Part 1: Comparing increasing/decreasing lists
 - Part 2: Same, but up to one item removed.
+
+## Day 03:
+- Part 1: Regex find and do `mul(x, y)` 
+- Part 2: Same but with added `do()` and `don't()` to do an instruction.
---
+++ day03/day03.py
@@ -0,0 +1,29 @@
+import re
+
+
+def part1(data):
+    res = 0
+    for x in re.finditer(r"mul\((\d+),(\d+)\)", data):
+        res += int(x.group(1)) * int(x.group(2))
+    return res
+
+
+def part2(data):
+    res = 0
+    do = True
+    for x in re.finditer(r"(don\'t\(\))|(do\(\))|mul\((\d+),(\d+)\)", data):
+        if x.group(0) == "don't()":
+            do = False
+        elif x.group(0) == "do()":
+            do = True
+        else:
+            if do:
+                res += int(x.groups()[2]) * int(x.groups()[3])
+    return res
+
+
+if __name__ == "__main__":
+    lines = open("big.txt", "r").readlines()
+    lines = "".join(lines)
+    print("Part 1:", part1(lines))
+    print("Part 2:", part2(lines))
