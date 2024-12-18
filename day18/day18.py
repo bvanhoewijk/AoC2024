9b86f0fd4a6364639c73aaecdd6c53baedf4232d
Bas van Hoewijk <basvanhoewijk@gmail.com>
2024-12-18 13:05:49 +0100

    day18

--- day18/day18.py
+++ day18/day18.py
@@ -1,13 +1,4 @@
-import math
-from queue import Queue
-
-def part1(dataset):
-    pass
-
-
-def part2(dataset):
-    pass
-
+from tqdm import tqdm
 
 def in_bounds(next_node, dataset_size):
     if next_node[0] < 0 or next_node[1] < 0:
@@ -26,29 +17,12 @@ dirs = [
     (0, -1),  # Left    <
 ]
 
-
-def print_grid(grid_cols, grid_rows, occupied, path):
-    for col in range(grid_cols):
-        for row in range(grid_rows):
-            if (row, col) in occupied:
-                print("#", end="")
-            elif (row, col) in path:
-                print("O", end="")
-            else:
-                print(".", end="")
-        print()
-    print()
-
-def manhattan(a, b):
-    return sum(abs(val1-val2) for val1, val2 in zip(a,b))
-
 def bfs(start, end, occupied, grid_cols, grid_rows):
     visited = set(start)
-    queue = Queue()
-    queue.put((start, 0))
-
-    while not queue.empty():
-        current, cost = queue.get()
+    queue = list()
+    queue.append((start, 0))
+    while queue:
+        current, cost = queue.pop(0)
         r, c = current
         for dr, dc in dirs:
             next_dir = (r + dr, c + dc)
@@ -58,34 +32,82 @@ def bfs(start, end, occupied, grid_cols, grid_rows):
                 return cost + 1
             else:
                 visited.add(next_dir)
-                queue.put((next_dir, cost+1))
-        
-def main():
-    # Small:
+                queue.append((next_dir, cost+1))
+    return None
+
+def part1():
+    print("SMALL DATASET")
     occupied = [
         tuple(map(int, item.strip().split(",")))
         for item in open("small.txt", "r").readlines()
     ]
     start = (0, 0)
     end = (6, 6)
-    memory_space = (6, 6)
-    cost = bfs(start, end, occupied[:12], memory_space[0]+1, memory_space[1]+1)
-    # print_grid(memory_space[0]+1, memory_space[1]+1, occupied=occupied[:12], path=path)
+    cost = bfs(start, end, occupied[:12], end[0]+1, end[1]+1)
     print(cost)
-    # # Big:
+    
+    ############################################################
+
+    print("BIG DATASET")
     occupied = [
         tuple(map(int, item.strip().split(",")))
         for item in open("big.txt", "r").readlines()
     ]
     start = (0, 0)
     end = (70, 70)
-    memory_space = (70, 70)
-    # 1108 is too high
-    # 285 incorrect
-    # 320 incorrect
-    # 268?
-    cost = bfs(start, end, occupied[:1024], memory_space[0]+1, memory_space[1]+1)
+    # 268
+    cost = bfs(start, end, occupied[:1024], end[0]+1, end[1]+1)
     print(cost)
+
+def part2():
+    print("SMALL DATASET")
+    occupied = [
+        tuple(map(int, item.strip().split(",")))
+        for item in open("small.txt", "r").readlines()
+    ]
+    start = (0, 0)
+    end = (6, 6)
+    occupied_sel = occupied[:12].copy()
+    rest = occupied[12:].copy()
+
+    for item in rest: 
+        occupied_sel.append(item)   
+        cost = bfs(start, end, occupied_sel,  end[0]+1, end[1]+1)
+        if cost == None:
+            print(item)
+            break
+        else:
+            cost
+    
+    ############################################################
+
+    print("BIG DATASET")
+    occupied = [
+        tuple(map(int, item.strip().split(",")))
+        for item in open("big.txt", "r").readlines()
+    ]
+    start = (0, 0)
+    end = (70, 70)
+    
+    occupied_sel = occupied[:1024].copy()
+    rest = occupied[1024:].copy()
+
+    for item in tqdm(rest):
+        occupied_sel.append(item)   
+        cost = bfs(start, end, occupied_sel,  end[0]+1, end[1]+1)
+        if cost == None:
+            print("Part 2", item)
+            break
+
+
+def main():
+    # 268
+    part1()
+
+    # 64,11
+    part2()
+  
+  
 
 if __name__ == "__main__":
     main()
